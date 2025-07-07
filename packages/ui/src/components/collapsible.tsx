af2961a22325b9357530aef154e465c8c7166ab5
nuicoder <trongson18101999@gmail.com>
2025-07-07 17:10:51 +0700

    feat: add new component

---
+++ packages/ui/src/components/collapsible.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import * as CollapsiblePrimitive from '@radix-ui/react-collapsible'
+
+type CollapsibleProps = React.ComponentProps<typeof CollapsiblePrimitive.Root>
+
+function Collapsible({ ...props }: CollapsibleProps) {
+  return <CollapsiblePrimitive.Root data-slot="collapsible" {...props} />
+}
+
+type CollapsibleTriggerProps = React.ComponentProps<
+  typeof CollapsiblePrimitive.CollapsibleTrigger
+>
+
+function CollapsibleTrigger({ ...props }: CollapsibleTriggerProps) {
+  return (
+    <CollapsiblePrimitive.CollapsibleTrigger
+      data-slot="collapsible-trigger"
+      {...props}
+    />
+  )
+}
+
+type CollapsibleContentProps = React.ComponentProps<
+  typeof CollapsiblePrimitive.CollapsibleContent
+>
+
+function CollapsibleContent({ ...props }: CollapsibleContentProps) {
+  return (
+    <CollapsiblePrimitive.CollapsibleContent
+      data-slot="collapsible-content"
+      {...props}
+    />
+  )
+}
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent }
+export type {
+  CollapsibleProps,
+  CollapsibleTriggerProps,
+  CollapsibleContentProps,
+}
